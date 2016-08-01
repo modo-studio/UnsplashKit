53c34641e7dcef22e4583ce216687b989e91c446
Sergi Gracia <sergigram@gmail.com>
2016-08-01 22:29:40 +0200

    Moved OSX support to typealias

--- UnsplashKit/Classes/Adapters/UrlDataResponseAdapter.swift
+++ UnsplashKit/Classes/Adapters/UrlDataResponseAdapter.swift
@@ -2,33 +2,20 @@ import Foundation
 import Core
 
 #if os(OSX)
-    
-    public class UrlDataResponseAdapter: Adapter<(data: NSData?, response: NSURLResponse?), (NSImage, NSURLResponse?)> {
-        
-        // MARK: - Public
-        
-        public override func adapt(input: (data: NSData?, response: NSURLResponse?)) -> (NSImage, NSURLResponse?)! {
-            guard let data = input.data, let image = NSImage(data: data) else {
-                return (NSImage(), input.response)
-            }
-            return (image, input.response)
-        }
-        
-    }
-    
+    public typealias UnsplashImage = NSImage
 #else
+    public typealias UnsplashImage = UIImage
+#endif
+
+public class UrlDataResponseAdapter: Adapter<(data: NSData?, response: NSURLResponse?), (UnsplashImage, NSURLResponse?)> {
     
-    public class UrlDataResponseAdapter: Adapter<(data: NSData?, response: NSURLResponse?), (UIImage, NSURLResponse?)> {
-        
-        // MARK: - Public
-        
-        public override func adapt(input: (data: NSData?, response: NSURLResponse?)) -> (UIImage, NSURLResponse?)! {
-            guard let data = input.data, let image = UIImage(data: data) else {
-                return (UIImage(), input.response)
-            }
-            return (image, input.response)
+    // MARK: - Public
+    
+    public override func adapt(input: (data: NSData?, response: NSURLResponse?)) -> (UnsplashImage, NSURLResponse?)! {
+        guard let data = input.data, let image = UnsplashImage(data: data) else {
+            return (UnsplashImage(), input.response)
         }
-        
+        return (image, input.response)
     }
     
-#endif
+}
